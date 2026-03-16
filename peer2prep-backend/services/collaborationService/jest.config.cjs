16fc91207ce328dc4e59a0d8e983ef12070ce0ff
varsha13152 <varsha13152@gmail.com>
2026-03-16 23:43:54 +0800

    setup files

---
+++ peer2prep-backend/services/collaborationService/jest.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+    preset: "ts-jest/presets/default-esm",
+    testEnvironment: "node",
+    extensionsToTreatAsEsm: [".ts"],
+    setupFiles: ["<rootDir>/jest.setup.cjs"],
+    moduleNameMapper: {
+        "^@/(.*)\\.js$": "<rootDir>/src/$1.ts",
+        "^@/(.*)$": "<rootDir>/src/$1",
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+    transform: {
+        "^.+\\.ts$": [
+            "ts-jest",
+            {
+                useESM: true,
+                tsconfig: "<rootDir>/tsconfig.json",
+            },
+        ],
+    },
+    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+};
