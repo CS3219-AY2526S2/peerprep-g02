817cdf014472f8fe67923771461b43b67b361fe6
hsinyilow <hsinyilow@gmail.com>
2026-03-17 23:14:34 +0800

    Add essential files for base question service
    Base essential files for question service features without the optional features or support functions.

---
+++ peer2prep-backend/services/questionService/src/routes/routes.ts
@@ -0,0 +1,90 @@
+import { Router } from "express";
+import {GetQuestion, EditQuestion, CreateQuestion, DeleteQuestion, GetQuestions, GetPopularQuestions} from "../services/questionDatabase"
+import express from 'express';
+
+
+const router = Router();
+
+
+//Get question
+router.get("/questions", async (req, res) => {
+    const result = await GetQuestions();
+    return res.status(200).json({
+        message: "Get success",
+        body: result
+    })
+})
+
+router.get("/questions/popular", async (req, res) => {
+    const result = await GetPopularQuestions();
+    return res.status(200).json({
+        message: "Get success",
+        body: result
+    })
+})
+
+
+
+//Save question
+router.post("/questions/get", async (req, res) => {
+    var result = await GetQuestion(req.body.quid);
+    
+    if (!result) {
+        return res.status(400).json({
+            message: "Unable to add question to database"
+        })
+    }
+
+    return res.status(200).json({
+        message: "Get success",
+        body: result
+    })
+})
+
+
+
+//Save question
+router.post("/questions", async (req, res) => {
+    var result = await CreateQuestion(req.body);
+    var result = true;
+    if (!result) {
+        return res.status(400).json({
+            message: "Unable to add question to database"
+        })
+    }
+
+    return res.status(200).json({
+        message: "Question successfully added to database"
+    })
+})
+
+router.put("/questions", async (req, res) => {
+    var result = false;
+    var result = await EditQuestion(req.body);
+
+    if (!result) {
+        return res.status(400).json({
+            message: "Unable to add question to database"
+        })
+    }
+        
+    return res.status(200).json({
+        message: "Question successfully edited in the database"
+    })
+})
+
+router.post("/questions/delete", async (req, res) => {
+    const result = await DeleteQuestion(req.body.quid);
+
+    if (!result) {
+        return res.status(400).json({
+            message: "Unable to add question to database"
+        })
+    }
+    
+    return res.status(200).json({
+        message: "Question successfully deleted from the database"
+    })
+})
+
+export default router;
---
+++ peer2prep-frontend/src/components/admin/AdminType.tsx
@@ -0,0 +1,36 @@
+import { UUID } from "node:crypto";
+
+export type QuestionInfo = {
+    quid: UUID,
+    title: string,
+    topics: string[],
+    difficulty: string
+}
+
+export type QuestionData = {
+    quid: UUID,
+    title: string,
+    topics: string[],
+    difficulty: string,
+    testCase: TestCase[],
+    description: string
+}
+
+export interface FormData {
+  qnTitle: string;
+  qnDesc: string;
+  testCase: TestCase[];
+  qnImage?: File | null;
+  difficulty: Difficulty;
+  qnTopics: string;  
+}
+
+export interface TestCase {
+    input: string;
+    output: string;
+}
+
+export enum Difficulty {
+    EASY = "Easy", 
+    MEDIUM = "Medium", 
+    HARD = "Hard"}
