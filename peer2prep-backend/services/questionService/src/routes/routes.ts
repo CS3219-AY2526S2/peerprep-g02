0c5c532c3a8ec5088d64fdc29861a700df37ee06
kevinlimantoro123 <kevinlimantoro123@gmail.com>
2026-03-18 03:31:35 +0800

    fix: GET /questions and GET/questions/popular are now routes for all users. The rest of the routes still require admin authentication

--- peer2prep-backend/services/questionService/src/routes/routes.ts
+++ peer2prep-backend/services/questionService/src/routes/routes.ts
@@ -1,49 +1,51 @@
 import { Router } from "express";
-import {GetQuestion, EditQuestion, CreateQuestion, DeleteQuestion, GetQuestions, GetPopularQuestions} from "../services/questionDatabase"
-import express from 'express';
+import {
+    GetQuestion,
+    EditQuestion,
+    CreateQuestion,
+    DeleteQuestion,
+    GetQuestions,
+    GetPopularQuestions,
+} from "../services/questionDatabase";
+import express from "express";
 import { requireAdminAuth } from "../middlewares/requireAdminAuth";
 
-
 const router = Router();
-
-router.use(requireAdminAuth);
 
 //Get question
 router.get("/questions", async (req, res) => {
     const result = await GetQuestions();
     return res.status(200).json({
         message: "Get success",
-        body: result
-    })
-})
+        body: result,
+    });
+});
 
 router.get("/questions/popular", async (req, res) => {
     const result = await GetPopularQuestions();
     return res.status(200).json({
         message: "Get success",
-        body: result
-    })
-})
+        body: result,
+    });
+});
 
-
+router.use(requireAdminAuth);
 
 //Save question
 router.post("/questions/get", async (req, res) => {
     var result = await GetQuestion(req.body.quid);
-    
+
     if (!result) {
         return res.status(400).json({
-            message: "Unable to add question to database"
-        })
+            message: "Unable to add question to database",
+        });
     }
 
     return res.status(200).json({
         message: "Get success",
-        body: result
-    })
-})
-
-
+        body: result,
+    });
+});
 
 //Save question
 router.post("/questions", async (req, res) => {
@@ -51,14 +53,14 @@ router.post("/questions", async (req, res) => {
     var result = true;
     if (!result) {
         return res.status(400).json({
-            message: "Unable to add question to database"
-        })
+            message: "Unable to add question to database",
+        });
     }
 
     return res.status(200).json({
-        message: "Question successfully added to database"
-    })
-})
+        message: "Question successfully added to database",
+    });
+});
 
 router.put("/questions", async (req, res) => {
     var result = false;
@@ -66,27 +68,27 @@ router.put("/questions", async (req, res) => {
 
     if (!result) {
         return res.status(400).json({
-            message: "Unable to add question to database"
-        })
+            message: "Unable to add question to database",
+        });
     }
-        
+
     return res.status(200).json({
-        message: "Question successfully edited in the database"
-    })
-})
+        message: "Question successfully edited in the database",
+    });
+});
 
 router.post("/questions/delete", async (req, res) => {
     const result = await DeleteQuestion(req.body.quid);
 
     if (!result) {
         return res.status(400).json({
-            message: "Unable to add question to database"
-        })
+            message: "Unable to add question to database",
+        });
     }
-    
+
     return res.status(200).json({
-        message: "Question successfully deleted from the database"
-    })
-})
+        message: "Question successfully deleted from the database",
+    });
+});
 
 export default router;
