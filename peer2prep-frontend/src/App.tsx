1614f86ac545aeac76fd53f96295bd21ea4dcaec
kevinlimantoro123 <kevinlimantoro123@gmail.com>
2026-03-20 01:58:41 +0800

    fix: users who exit and then open the session again will get redirected to "/home" instead of "/" if authenticated. if not, they get redirected to sign in page

--- peer2prep-frontend/src/App.tsx
+++ peer2prep-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ProtectedRoute } from "@/components/authRoute/ProtectedRoute";
 import { ROUTES } from "@/constants/routes";
 
@@ -18,6 +18,7 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Navigate to={ROUTES.DASHBOARD} replace />} />
                 <Route path={`${ROUTES.LOGIN}/*`} element={<LoginView />} />
                 <Route path={`${ROUTES.REGISTER}/*`} element={<RegisterView />} />
 
